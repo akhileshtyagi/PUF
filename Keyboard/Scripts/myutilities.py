a1ef7d2de29cb5f9030b6398710803b8b30b2646
Ian Richardson <iantrich@gmail.com>
2015-04-09 14:57:47 -0500

    Started adding better documentation to myutilities class

--- Keyboard/Scripts/myutilities.py
+++ Keyboard/Scripts/myutilities.py
@@ -10,10 +10,23 @@ from decimal import *
 PRECISION = 50
 
 
+########################################################
+# Returns the absolute directory path from which the
+# script is being ran
+#
+# @return: String
+########################################################
 def get_current_dir():
     return os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
 
+########################################################
+# @param(path): String representation of an absolute path
+#
+# Returns a list of directories in a given path
+#
+# @return: List of Strings
+########################################################
 def list_dirs(path):
     dirs_list = []
     for dirs in os.listdir(path):
@@ -21,6 +34,13 @@ def list_dirs(path):
     return dirs_list
 
 
+########################################################
+# @param(path): String representation of an absolute path
+#
+# Returns a List of files in a given path
+#
+# @return: List of Strings
+########################################################
 def list_files(path):
     files_list = []
     for files in os.listdir(path):
@@ -28,11 +48,26 @@ def list_files(path):
     return files_list
 
 
+########################################################
+# @param(selections): List of Strings
+#
+# Prints selections as a list
+#
+# @return:
+########################################################
 def print_selections(selections):
     for i, selection in enumerate(selections):
         print(str(i) + '. ' + str(selection) + '\n')
 
 
+########################################################
+# @param(options): List of numbers
+#
+# Type-checks user input to be an integer within the
+# valid option range and returns the choice
+#
+# @return: integer
+########################################################
 def grab_valid_input(options):
     choice = int(raw_input())
     while 0 > choice > len(options):
@@ -40,15 +75,38 @@ def grab_valid_input(options):
     return choice
 
 
+########################################################
+# @param(path): String representation of an absolute path
+#
+# Create directories to make that absolute path valid in
+# the file system in not already present
+#
+# @return:
+########################################################
 def create_dir_path(path):
-    # Make directories user/device/date/window and each token within that token
     if not os.path.exists(path):
         os.makedirs(path)
 
 
-def normalize_raw_element(keycode, pressure, dist):
-    distribution = dist[0]
-    keycode_dist = dist[1]
+########################################################
+# @param(keycode): integer
+# @param(pressure): float
+# @param(distribution): List of distributions
+# containing a dictionary in
+# each index with keys 'lower' and 'upper'
+# @param(keycode_dist): List of distributions for each
+# keycode with a dictionary in each index with keys
+# 'lower', 'upper', 'mean' and 'std'
+#
+# TODO
+#
+# @return: integer representing the index that the given
+# pressure value for the given keycode falls within
+# and -1 if not
+########################################################
+def normalize_raw_element(keycode, pressure, distribution, keycode_dist):
+    # distribution = dist[0]
+    # keycode_dist = dist[1]
     if keycode < 0:
         keycode = 0
     if pressure < keycode_dist[keycode].get('lower') or pressure > keycode_dist[keycode].get('upper'):
@@ -62,6 +120,13 @@ def normalize_raw_element(keycode, pressure, dist):
     return len(distribution) - 1
 
 
+########################################################
+# @param(current_window): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 def hash_function(current_window):
     hashcode = float(0.0)
     for i, touch in enumerate(current_window):
@@ -71,6 +136,14 @@ def hash_function(current_window):
     return hashcode
 
 
+########################################################
+# @param(hashcode_bin): TODO
+# @param(current_window): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 def match_sequence(hashcode_bin, current_window):
     if hashcode_bin is None:
         return -1
@@ -86,6 +159,17 @@ def match_sequence(hashcode_bin, current_window):
     return -1
 
 
+########################################################
+# @param(hashcode): TODO
+# @param(hashcode_bin): TODO
+# @param(link_index): TODO
+# @param(current_window): TODO
+# @param(table): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 # Given the index of the node in the bin, increment the value of current_window[len(current_window) - 1]
 def increment_probability(hashcode, hashcode_bin, link_index, current_window, table):
     hashcode_bin.get('chain')[link_index].get('probabilities')[current_window[-1][1]] += 1
@@ -94,6 +178,17 @@ def increment_probability(hashcode, hashcode_bin, link_index, current_window, ta
     return table
 
 
+########################################################
+# @param(hashcode): TODO
+# @param(hashcode_bin): TODO
+# @param(current_window): TODO
+# @param(table): TODO
+# @param(token): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 # Add link to bin with {'sequence': current_window[0...n-1] and current_window[n-1]: 1}
 def add_link(hashcode, hashcode_bin, current_window, table, token):
     lst = [0] * token
@@ -109,6 +204,16 @@ def add_link(hashcode, hashcode_bin, current_window, table, token):
     return table
 
 
+########################################################
+# @param(hashcode): TODO
+# @param(current_window): TODO
+# @param(table): TODO
+# @param(token): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 # Add a bin with {'chain': [{'sequence': current_window[0...n-1] and current_window[n-1]: 1}]}
 def add_key(hashcode, current_window, table, token):
     lst = [0] * token
@@ -123,10 +228,26 @@ def add_key(hashcode, current_window, table, token):
     return table
 
 
+########################################################
+# @param(hashcode_bin): TODO
+# @param(current_window): TODO
+# @param(link_index): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 def touch_probability(hashcode_bin, current_window, link_index):
     return hashcode_bin.get('chain')[link_index].get('probabilities')[current_window[-1][1]]
 
 
+########################################################
+# @param(table): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 def convert_table_to_probabilities(table):
     getcontext().prec = PRECISION
     for key, val in table.items():
@@ -136,6 +257,13 @@ def convert_table_to_probabilities(table):
     return table
 
 
+########################################################
+# @param(read): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 def find_max_min(read):
     minimum = 1.0
     maximum = 0.0
@@ -147,6 +275,14 @@ def find_max_min(read):
     return {'max': maximum, 'min': minimum}
 
 
+########################################################
+# @param(raw_data_file): TODO
+# @param(token): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 # Generate a clustering distribution and return as tuple ranges
 def cluster_algorithm(raw_data_file, token):
     with open(raw_data_file, 'rt') as csvfile:
@@ -172,6 +308,13 @@ def cluster_algorithm(raw_data_file, token):
     return [distribution, key_dist]
 
 
+########################################################
+# @param(reader): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 # Keycode distributions
 def keycode_distribution(reader):
     data = [[] for i in range(126)]
@@ -193,6 +336,19 @@ def keycode_distribution(reader):
     return distribution
 
 
+########################################################
+# @param(raw_data_file): TODO
+# @param(table): TODO
+# @param(distribution): TODO
+# @param(window): TODO
+# @param(threshold): TODO
+# @param(token): TODO
+# @param(match_user): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 def build_lookup(raw_data_file, table, distribution, window, threshold, token, match_user):
     normalized = []
     current_window = []
@@ -251,6 +407,19 @@ def build_lookup(raw_data_file, table, distribution, window, threshold, token, m
         return table
 
 
+########################################################
+# @param(raw_data_file): TODO
+# @param(base_table): TODO
+# @param(distribution): TODO
+# @param(window): TODO
+# @param(threshold): TODO
+# @param(token): TODO
+# @param(n): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 def build_auth_table(raw_data_file, base_table, distribution, window, threshold, token, n):
     normalized = []
     current_window = []
@@ -330,6 +499,14 @@ def build_auth_table(raw_data_file, base_table, distribution, window, threshold,
     return probabilities
 
 
+########################################################
+# @param(base): TODO
+# @param(auth): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 def compare(base, auth):
     # Sum of probabilities
     s = 0
@@ -371,6 +548,15 @@ def compare(base, auth):
     return [s, n]
 
 
+########################################################
+# @param(auth): TODO
+# @param(base): TODO
+# @param(current): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 def get_oldest(auth, base, current):
     getcontext().prec = 4
     auth_hashcode_bin = auth.get(hash_function(current))
@@ -387,6 +573,15 @@ def get_oldest(auth, base, current):
         return 0 - auth_prob
 
 
+########################################################
+# @param(auth): TODO
+# @param(base): TODO
+# @param(current): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 def get_newest(auth, base, current):
     getcontext().prec = 4
     auth_hashcode_bin = auth.get(hash_function(current))
@@ -403,6 +598,14 @@ def get_newest(auth, base, current):
         return 0 - auth_prob
 
 
+########################################################
+# @param(table): TODO
+# @param(current): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 def remove_oldest(table, current):
     hashcode = hash_function(current)
     hashcode_bin = table.get(hashcode)
@@ -420,6 +623,14 @@ def remove_oldest(table, current):
     return table
 
 
+########################################################
+# @param(base_sequence): TODO
+# @param(auth_chain): TODO
+#
+# TODO
+#
+# @return: TODO
+########################################################
 def find_sequence(base_sequence, auth_chain):
     for i, link in enumerate(auth_chain):
         if base_sequence == link.get('sequence'):
