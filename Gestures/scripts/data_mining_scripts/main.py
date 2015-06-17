1cca7def388ca712d27edae8556e04ae82c189f5
timdee <timdee@iastate.edu>
2015-06-17 06:08:39 -0400

    some more stuff added to machine learning scripts

--- Gestures/scripts/data_mining_scripts/main.py
+++ Gestures/scripts/data_mining_scripts/main.py
@@ -1,13 +1,35 @@
 #! /bin/python2
 
 import trainer
+import challenge_response
 
 ######main.py
 #Provides the ability to:
 #1) run all machine learning tests
 #2) run each of the machine learning tests individually
 def main():
-	while not (selection == 42):
-		selection = 42
-		switch(selection):
-			
+	prompt = "Please select an option:\n"
+	prompt = prompt + "0) run all tests\n"
+	prompt = prompt + "1) support vector classification\n"
+	prompt = prompt + "42) exit\n"
+
+	#TODO create responses for all data sets for a given (user, challenge)	
+	response_set = []
+	
+	
+	worker = trainer.trainer(response_set)
+	selection = -1
+
+	while selection != 42:
+		selection = input(prompt)
+		
+		if selection == 0:
+			#TODO run all tests
+			print worker.classify_svm()
+
+		if selection == 1:
+			print worker.classify_svm()
+
+
+#finally, call the main method
+main()
--- Gestures/scripts/data_mining_scripts/trainer.py
+++ Gestures/scripts/data_mining_scripts/trainer.py
@@ -6,7 +6,7 @@ from sklearn import svm
 #Handles the training of the machine learning models. Offers methods to run #different types of machine learning tests, and returns their results.
 class trainer:
 	#sets up the data used to fit any given model. expects a response object.
-	def __init__(self, response):
+	def __init__(self, response_set):
 		#TODO set up the digits object
 		self.digits = datasets.load_digits()
 
@@ -20,3 +20,7 @@ class trainer:
 		clf.fit(self.digits.data[:-1], self.digits.target[:-1])
 		#use the model to predict the next thing
 		clf.predict(digits.data[-1])
+
+
+	#TODO implement more machine learning algorithms and see how they do
+	#TODO write a function to determine the accuracy of a given prediction model given N data sets; graph this data? is there a python library for graphing?
