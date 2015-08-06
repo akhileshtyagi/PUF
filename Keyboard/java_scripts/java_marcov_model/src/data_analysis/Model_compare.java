35e0959e8f4168cda3045a47f2b8d770b9da414b
timdee <timdee@iastate.edu>
2015-08-06 12:12:03 -0400

    new test to run on desktop

--- Keyboard/java_scripts/java_marcov_model/src/data_analysis/Model_compare.java
+++ Keyboard/java_scripts/java_marcov_model/src/data_analysis/Model_compare.java
@@ -20,8 +20,8 @@ public class Model_compare {
 	private final static String input_folder_name = "data_sets";
 	
 	//specify different model sizes and 
-	private final static int[] window_sizes = {2,3,4,5};
-	private final static int[] token_sizes = {3,4,5,6,7,8,9,10};
+	private final static int[] window_sizes = {3};
+	private final static int[] token_sizes = {7};
 	private final static int[] thresholds = {5000};
 	private static int[] user_model_sizes = {2000,4000,6000,8000};//new int[10]; //5000
 	private static int[] auth_model_sizes = {2000,4000,6000,8000};//new int[10]; //4000
