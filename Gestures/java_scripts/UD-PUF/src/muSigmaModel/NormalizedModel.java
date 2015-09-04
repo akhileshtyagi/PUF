703ac954694b5bf76bbb5a0d2c70b5b99dd89a1d
timdee <timdee@iastate.edu>
2015-09-04 01:01:11 -0400

    worked on normalization component. Completed the first portion where the x,y points to normalize to are computed. Next to be completed is normalizing the responses to these points.

--- Gestures/java_scripts/UD-PUF/src/muSigmaModel/NormalizedModel.java
+++ Gestures/java_scripts/UD-PUF/src/muSigmaModel/NormalizedModel.java
@@ -14,6 +14,9 @@ import data.ChallengeResponse;
  *
  */
 public class NormalizedModel {
+    // number of elements in normalized list.
+    final int NORMALIZED_ELEMENTS = 32;
+
     // list of {(mu_x,mu_y,sigma_x,sigma_y), (mu_x,mu_y,sigma_x,sigma_y), ...}
     // it may only be {(mu,sigma), (mu,sigma), ...} becuase I think we only
     // compute mu,sigma for either x or y, not both
@@ -44,15 +47,175 @@ public class NormalizedModel {
     /**
      * creates the normalized challenge response list.
      * 
-     * @return
+     * @return {(x,y,pressure), (x,y,pressure), ...}
      */
     private List<List<Double>> normalizeChallengeResponseList(List<ChallengeResponse> challengeResponseList) {
 	ArrayList<List<Double>> normalizedList = new ArrayList<List<Double>>();
-	ArrayList<Double> normalizedElement = new ArrayList<Double>();
+	List<List<Double>> x_y_list;
+	ArrayList<Double> pressure_list = new ArrayList<Double>();
 
 	// TODO normalize the list
+	// we either normalize with respect to x or y
+	// we need to normalize x,y,pressure values
+	for (ChallengeResponse response : challengeResponseList) {
+	    // determine if the challenge is more horizontal or more vertical in
+	    // oreantation
+	    double x_dist = computeChallengeXDistance(response);
+	    double y_dist = computeChallengeYDistance(response);
+
+	    if (x_dist > y_dist) {
+		// normalize with respect to x
+		x_y_list = computeHorizontalPointsAlongChallenge(response, x_dist);
+
+		// TODO I think the next step is to figure out the closcest
+		// thing the the x_y_list points in the responses. We do
+		// something to align these points to get our normalized list.
+	    } else {
+		// normalize with respect to y
+		x_y_list = computeVerticalPointsAlongChallenge(response, y_dist);
+
+		// TODO I think the next step is to figure out the closcest
+		// thing the the x_y_list points in the responses. We do
+		// something to align these points to get our normalized list.
+	    }
+	    // TODO add the element to normalizedList
+	}
 
 	return normalizedList;
+    }
+
+    /**
+     * return a list of NORMALIZED_ELEMENTS number of points horizontally along
+     * the challenge. We will normalize with respect to these points.
+     */
+    private List<List<Double>> computeHorizontalPointsAlongChallenge(ChallengeResponse response, double x_dist) {
+	ArrayList<List<Double>> points = new ArrayList<List<Double>>();
+
+	// amount of x separation for each point
+	double point_increment = x_dist / NORMALIZED_ELEMENTS;
+
+	for (int i = 0; i < NORMALIZED_ELEMENTS; i++) {
+	    List<Double> next_point = new ArrayList<Double>();
+
+	    // do something different for the first iteration
+	    if (points.size() == 0) {
+		// use the x,y values of the first point
+		next_point.add(response.getChallengeList().get(0).get(0)); // x
+									   // value
+		next_point.add(response.getChallengeList().get(0).get(1)); // y
+									   // value
+	    } else {
+		// add point increment to the y value while staying at the same
+		// x value
+		next_point.add(points.get(i - 1).get(0) + point_increment); // x
+									    // value
+		next_point.add(points.get(i - 1).get(1)); // y
+							  // value
+	    }
+
+	    points.add(next_point);
+	}
+
+	return points;
+
+    }
+
+    /**
+     * return a list of NORMALIZED_ELEMENTS number of points vertically along
+     * the challenge. We will normalize with respect to these points.
+     */
+    private List<List<Double>> computeVerticalPointsAlongChallenge(ChallengeResponse response, double y_dist) {
+	ArrayList<List<Double>> points = new ArrayList<List<Double>>();
+
+	// amount of y separation for each point
+	double point_increment = y_dist / NORMALIZED_ELEMENTS;
+
+	for (int i = 0; i < NORMALIZED_ELEMENTS; i++) {
+	    List<Double> next_point = new ArrayList<Double>();
+
+	    // do something different for the first iteration
+	    if (points.size() == 0) {
+		// use the x,y values of the first point
+		next_point.add(response.getChallengeList().get(0).get(0)); // x
+									   // value
+		next_point.add(response.getChallengeList().get(0).get(1)); // y
+									   // value
+	    } else {
+		// add point increment to the y value while staying at the same
+		// x value
+		next_point.add(points.get(i - 1).get(0)); // x value
+		next_point.add(points.get(i - 1).get(1) + point_increment); // y
+									    // value
+	    }
+
+	    points.add(next_point);
+	}
+
+	return points;
+    }
+
+    /**
+     * returns true if the given points cover more horizontal distance compared
+     * to vertical distance. "given points" are the challenge list.
+     */
+    private boolean isHorizontalLine(ChallengeResponse response) {
+	return computeChallengeXDistance(response) > computeChallengeYDistance(response);
+    }
+
+    /**
+     * computes the y distance covered by a challenge
+     * 
+     * @param response
+     * @return
+     */
+    private double computeChallengeYDistance(ChallengeResponse response) {
+	double y_dist = 0;
+
+	// calculate x_dist and y_dist covered by the list
+	List<Double> prev_challenge_point = null;
+
+	for (List<Double> challenge_point : response.getChallengeList()) {
+	    if (prev_challenge_point == null) {
+		prev_challenge_point = challenge_point;
+		continue;
+	    }
+
+	    // compute the distance between the current point and the previous
+	    // point
+	    y_dist += Math.abs(challenge_point.get(1) - prev_challenge_point.get(1));
+
+	    prev_challenge_point = challenge_point;
+	}
+
+	return y_dist;
+    }
+
+    /**
+     * computes the x distance covered by a challenge
+     * 
+     * @param response
+     * @return
+     */
+    private double computeChallengeXDistance(ChallengeResponse response) {
+	double x_dist = 0;
+
+	// calculate x_dist and y_dist covered by the list
+	List<Double> prev_challenge_point = null;
+
+	for (List<Double> challenge_point : response.getChallengeList()) {
+	    if (prev_challenge_point == null) {
+		prev_challenge_point = challenge_point;
+		continue;
+	    }
+
+	    // compute the distance between the current point and the previous
+	    // point
+	    x_dist += Math.abs(challenge_point.get(0) - prev_challenge_point.get(0));
+
+	    prev_challenge_point = challenge_point;
+	}
+
+	return x_dist;
     }
 
     /**
