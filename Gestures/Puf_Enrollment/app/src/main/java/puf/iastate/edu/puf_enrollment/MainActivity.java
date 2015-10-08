7a44054fe0d75ad601bfbcd1218777c5fe4b3369
Nick Montelibano <nmont@iastate.edu>
2015-10-08 00:26:35 -0500

    Adding Main Android app project

---
+++ Gestures/Puf_Enrollment/app/src/main/java/puf/iastate/edu/puf_enrollment/MainActivity.java
@@ -0,0 +1,24 @@
+package puf.iastate.edu.puf_enrollment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void enroll_pressed(View v) {
+        Toast.makeText(this, "Launching Enrollment", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void authenticate_pressed(View v) {
+        Toast.makeText(this, "Launching Authentication", Toast.LENGTH_SHORT).show();
+    }
+}
