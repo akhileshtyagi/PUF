c8b8d86d306a1626e2daec23008ac2096646e880
nmont <nmont16@gmail.com>
2015-11-12 04:41:45 -0600

    Added new library, profile creation and persistent profiles stored in shared preferences

--- Gestures/Puf_Enrollment/app/src/main/java/puf/iastate/edu/puf_enrollment/RegisterGesturesActivity.java
+++ Gestures/Puf_Enrollment/app/src/main/java/puf/iastate/edu/puf_enrollment/RegisterGesturesActivity.java
@@ -1,7 +1,10 @@
 package puf.iastate.edu.puf_enrollment;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Environment;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,9 +16,17 @@ import com.opencsv.CSVWriter;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+
+import data.DataReader;
+import dataTypes.Challenge;
+import dataTypes.Profile;
+import dataTypes.Response;
+
+import com.google.gson.Gson;
 
 public class RegisterGesturesActivity extends AppCompatActivity implements PufDrawView.ResponseListener{
 
@@ -29,6 +40,10 @@ public class RegisterGesturesActivity extends AppCompatActivity implements PufDr
     private TextView mSeedView;
     private TextView mRemainingView;
     private PufDrawView mPdv;
+
+    private Challenge mChallenge;
+    private ArrayList<Response> mResponses;
+    private ArrayList<dataTypes.Point> mChallengePoints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +69,8 @@ public class RegisterGesturesActivity extends AppCompatActivity implements PufDr
         //Setup an initial challenge and give the challenge
         mCurChallenge = mCg.generateChallenge();
         mPdv.giveChallenge(mCurChallenge.toArray(new Point[mCurChallenge.size()]));
+        mResponses = new ArrayList<>();
+        mChallengePoints = new ArrayList<>();
 
         //Initialize remaining swipes
         mRemainingSwipes = 20;
@@ -68,7 +85,20 @@ public class RegisterGesturesActivity extends AppCompatActivity implements PufDr
     public void onResponseAttempt(ArrayList<Point> response) {
         if(--mRemainingSwipes == 0)
         {
+            DataReader reader = new DataReader(new File(Environment.getExternalStorageDirectory() + "/PUFProfile"));
+            Gson gson = new Gson();
             Toast.makeText(this, "Completed Authentication", Toast.LENGTH_SHORT).show();
+//            Challenge challenge = reader.readDataDirecotry();
+            Challenge challenge = new Challenge(mChallengePoints,42);
+            for(Response r : mResponses) {
+                challenge.addResponse(r);
+            }
+            String json = gson.toJson(challenge,challenge.getClass());
+
+            SharedPreferences sharedPref = this.getSharedPreferences("puf.iastate.edu.puf_enrollment.profile", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString(getString(R.string.profile_string), json);
+            editor.commit();
             finish();
         }
         writeResponseCsv(response, "DeviceName", "UserName");
@@ -83,6 +113,9 @@ public class RegisterGesturesActivity extends AppCompatActivity implements PufDr
      */
     public void writeResponseCsv(ArrayList<Point> response, String deviceName, String testerName)
     {
+
+        ArrayList<dataTypes.Point> points = new ArrayList<>();
+
         //File baseDir = new File(getFilesDir(), "PUFProfile");
         File baseDir = new File(Environment.getExternalStorageDirectory(), "PUFProfile");
 
@@ -110,6 +143,8 @@ public class RegisterGesturesActivity extends AppCompatActivity implements PufDr
                         testerName,
                         deviceName };
                 csvWrite.writeNext(row);
+
+                if(mChallengePoints.size() < 4)mChallengePoints.add(new dataTypes.Point(point.x,point.y,0));
             }
 
             String[] headers = { "X", "Y", "PRESSURE" };
@@ -123,8 +158,9 @@ public class RegisterGesturesActivity extends AppCompatActivity implements PufDr
                         Float.toString(point.y),
                         Float.toString(point.pressure) };
                 csvWrite.writeNext(row);
+                points.add(new dataTypes.Point(point.x, point.y, point.pressure));
             }
-
+            mResponses.add(new Response(points));
             csvWrite.close();
 
             Toast.makeText(this, "Challenge response written to CSV.", Toast.LENGTH_SHORT).show();
