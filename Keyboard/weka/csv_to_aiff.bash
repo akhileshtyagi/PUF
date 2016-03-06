44bc68a5d7240d8edb707e0c2be464557a2fbe34
tim <deetimothy33@gmail.com>
2016-03-05 22:17:32 -0600

    fixed bad reference order

--- Keyboard/weka/csv_to_aiff.bash
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# The purpose of this script is to rename csv files to aiff files
-# usage: ./csv_to_aiff.bash "filename"
-for i in $*; do 
- mv "$i" "$i.arff"
- echo "moving $i to $i.arff" 
-done
