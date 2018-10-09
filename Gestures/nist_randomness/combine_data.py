5f222bb3ad8a9653bf919aa0e6dbe73faee456ed
timdee <deetimothy33@gmail.com>
2018-10-09 11:35:12 -0500

    stuck... randomness tests are taking a segmentation fault

--- Gestures/nist_randomness/combine_data.py
+++ Gestures/nist_randomness/combine_data.py
@@ -8,8 +8,22 @@ import os
 import sys
 from os.path import join, dirname, basename
 
-GENERATED_OUTPUT_DIR = "../ryan_data/ryan_data/OutputGenerated/"
+GENERATED_OUTPUT_DIR = "/home/element/PUF/Gestures/ryan_data/OutputGenerated/"
 RESULTS_OUTPUT_DIR = "data/"
+
+def bin_to_ascii(binary):
+    s=""
+    # convert binary string to ascii bit by bit
+    # one bit of the binary becomes a 1 or 0 in ascii
+    # input is a binary string
+    # each character is treated as 8 bit
+    #for c in binary:
+    #    character=int(c,2)
+    #    for i in range(8):
+    #        mask=2**i
+    #        s+= (character & mask)
+
+    return s.join(format(x,'b') for x in bytearray(binary))
 
 def main():
     strategy_map={}
@@ -36,10 +50,35 @@ def main():
             # append the file contents to the list
             with open(inputBinaryFile,'rb') as file:
                 strategy_map[stratName].append(file.read())
+
+    #TEST
+    #print strategy_map
+    print ord(strategy_map['Strat1'][0][1])
     
-    for k,v in strategy_map:
-        output=os.path.join(outputFile,k,'.bin')
-        with open(output,'wb') as file:
-            # write a combined output file for each strategy
-            for response in v:
-                file.write(response)
+    #TODO the data may NOT be getting output properly
+
+    # output as binary
+    for k in strategy_map:
+        output_bin=os.path.join(outputFile,k+'.bin')
+        output_ascii=os.path.join(outputFile,k+'.txt')
+        # output as binary
+        with open(output_bin,'wb') as file:
+            for v in strategy_map[k]:
+                # write a combined output file for each strategy
+                for response in v:
+                    file.write(response)
+        # also output as ASCII
+        with open(output_ascii,'w') as file:
+            for v in strategy_map[k]:
+                # write a combined output file for each strategy
+                for response in v:
+                    file.write(bin_to_ascii(response))
+
+    #TEST test that output is same as input
+    with open('data/Strat1.bin','rb') as file:
+        print ord(file.read()[1])
+
+
+
+#if __name__ == '__main__':
+main()
